455ebb210bcb895cad059c7179b74d23cc6bc620
Joaquín Sandoval M <joakas.11@gmail.com>
2022-06-08 14:21:26 -0500

    Create MatrizTranspuesta_naive.jl

---
+++ KernelAbstractions/MatrizTranspuesta_naive.jl
@@ -0,0 +1,13 @@
+using KernelAbstractions, CUDA
+
+@kernel function transpose_kernel!(A, @Const(B)
+    I, J = @index(Global, NTuple)
+    @inbounds A[I,J] = B[J,I]
+  end
+
+B = CUDA.rand(1024,1024)
+A = similar(B)
+
+const transpose! = transpose_kernel!(CUDADevice(),(32,32))
+event = transpose!(A,B, ndrange=size(A))
+wait(event)

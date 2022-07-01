7422ca9f4688049a1b3d71ec2d9c2496b68ee3cc
JOAQUIN SANDOVAL MIRAMONTES <joakas.11@gmail.com>
2022-06-30 20:15:35 -0500

    Naive Reduction Works. Test on test.jl

--- CUDA jl/Parallel Reduction/naiveReduction.jl
+++ CUDA jl/Parallel Reduction/naiveReduction.jl
@@ -4,7 +4,9 @@ using CUDA
 
 function naive_reduction_kernel(data_out, data_in, stride, size)
     idx = (blockIdx().x -1) * blockDim().x + threadIdx().x 
-    if (idx + stride < size)
+    if (idx + stride <= size)
+        #@cuprintln("ID = $idx; stride = $stride")
+        #@cuprintln("[$idx] = $(data_out[idx]) + [$idx + $stride]$(data_out[idx+stride])")
         data_out[idx] += data_in[idx + stride]
     end
     return nothing
@@ -12,12 +14,16 @@ end
 
 function naive_reduction(d_out, d_in, n_threads, size)
     n_blocks = div((size + n_threads -1) , n_threads)
+    
+    #while stride < size
+    for stride = 1:size
+        if log(2,stride) % 1 != 0
+            continue
+        end
 
-    global stride = 1
-    while stride < size
-        tmp = stride
-        @cuda threads=n_threads blocks=n_blocks naive_reduction_kernel(d_out, d_in, tmp, size)
-        global stride *= 2
+        @cuda threads=n_threads blocks=n_blocks naive_reduction_kernel(d_out, d_in, stride, size)
+        #global stride *= 2
+        
     end
 end
 
--- CUDA jl/Parallel Reduction/test.jl
+++ CUDA jl/Parallel Reduction/test.jl
@@ -1,19 +1,21 @@
 include("naiveReduction.jl")
-include("sharedMemoryReduction.jl")
+#include("sharedMemoryReduction.jl")
 
-A = zeros(128)
-B = similar(A)
+const size = 100
+
+A = zeros(size)
+
 
 for i = eachindex(A)
     A[i] = i
 end
 
 d_a = CuArray(A)
-d_b = CuArray(B)
-
-#naive_reduction(d_b, d_a, 32, length(A))
-reduction(d_b,d_a,32,length(A))
+d_b = d_a
+println(A)
+naive_reduction(d_b, d_a, 32, length(A))
+#reduction(d_b,d_a,32,length(A))
 
 B = Array(d_b)
 
-println(B)
+println("Sum from 1 to $size: $(B[1])")
